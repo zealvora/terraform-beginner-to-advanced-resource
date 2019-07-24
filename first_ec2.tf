0c931c333beb99b53447c8a7b7a38759a613d0d6
Zeal Vora <instructors@kplabs.in>
2019-07-24 21:29:19 +0530

    Create first_ec2.tf

---
+++ first_ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-d874e0a0"
+  instance_type = "t2.micro"
+}
