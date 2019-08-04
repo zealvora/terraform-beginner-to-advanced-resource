92c5ca8f6b274d84db30f0df7e010ad7da7ab359
Zeal Vora <instructors@kplabs.in>
2019-08-04 16:53:52 +0530

    Create varsdemo.tf

---
+++ section02/varsdemo.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "var_demo" {
+  name        = "kplabs-variables"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+}
