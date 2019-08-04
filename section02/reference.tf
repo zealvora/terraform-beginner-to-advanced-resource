c2ecf069cb96d7dd9b43771fa126096e463a8029
Zeal Vora <instructors@kplabs.in>
2019-08-04 16:56:39 +0530

    Create reference.tf

---
+++ section02/reference.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myec2.id
+  allocation_id = aws_eip.lb.id
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "kplabs-security-group"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+
+#    cidr_blocks = [aws_eip.lb.public_ip/32]
+  }
+}
