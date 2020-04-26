3b330fe00ca988422e1820acea8d05bec3e98c0a
Zeal Vora <instructors@kplabs.in>
2020-04-26 18:41:56 +0530

    Create state-management.tf

---
+++ Section 5 - Remote State Management/state-management.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region     = "us-west-2"
+    access_key = "YOUR-ACCESS-KEY"
+    secret_key = "YOUR-SECRET-KEY"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kplabs-remote-backends"
+    key    = "demo.tfstate"
+    region = "us-east-1"
+    access_key = "YOUR-ACCESS-KEY"
+    secret_key = "YOUR-SECRET-KEY"
+  }
+}
