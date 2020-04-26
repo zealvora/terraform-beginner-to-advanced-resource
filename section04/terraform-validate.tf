eed76488031b66c8ecf8ec399e5ebc55dea27a2d
Zeal Vora <instructors@kplabs.in>
2020-04-26 16:23:14 +0530

    Create terraform-validate.tf

---
+++ section04/terraform-validate.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "YOUR-ACCESS-KEY"
+  secret_key = "YOUR-SECRET-KEY"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = var.instancetype
+  sky = "blue"
+}
